43759f352d26e93a8477e83f1f6c9ddb49d432b9
Cordell <cordellbonnieux@gmail.com>
2022-02-01 22:07:04 -0800

    Fixed Tickets component.

--- src/Tickets.jsx
+++ src/Tickets.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Section from './Section';
 
-const ticketInfo1 = {
-  title: "ticket 1",
+const exampleInfo = {
+  title: "ticket",
   url: "www.example.com",
   bands: ["band 1", "band 2", "band 3", "etc"]
 }
 
 function Tickets() {
-  const ticket1 = ticketSection(ticketInfo1)
+  const ticket1 = ticketSection(exampleInfo);
+  const ticket2 = ticketSection(exampleInfo);
+  const ticket3 = ticketSection(exampleInfo);
+  const ticket4 = ticketSection(exampleInfo);
   const content = (
     <div className="tickets flex">
       {ticket1}
+      {ticket2}
+      {ticket3}
+      {ticket4}
     </div>
   )
 
@@ -20,25 +26,25 @@ function Tickets() {
   )
 }
 
-/*
-* Band list part needs to be re-worked
-*/
 function ticketSection(ticketInfo) {
   const { title, url, bands } = ticketInfo;
-  const bandList = (bands) => {
-    const ul = <ul />;
-    for (let i = 0; i < bands.length; i++) {
-      const li = <li>{bands[i]}</li>
-      ul.appendChild(li)
-    }
-    return ul
-  }
+  const bandList = bandsList(bands);
   return (
     <div className={`ticket ${title}`}> 
       <h3>{title}</h3>
       {bandList}
       <a href={url}>{url}</a>
     </div>
+  )
+}
+
+function bandsList(bands) {
+  return (
+    <ul>
+      {bands.map((band) => (
+        <li key={band}>{band}</li>
+      ))}
+    </ul>
   )
 }
 
