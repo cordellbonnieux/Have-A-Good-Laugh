99fac637e3d1a7920a8597df7e0280c6d9c54c19
Cordell <cordellbonnieux@gmail.com>
2022-02-04 19:36:22 -0800

    Removed dropped band, updated day 2 flyer, updated ticket section and added payment buttons.

--- src/Tickets.jsx
+++ src/Tickets.jsx
@@ -1,5 +1,11 @@
 import React from 'react'; 
 import Section from './Section';
+import ticketData from './data/tickets.json';
+// maybe i dont need these...
+import day1 from './images/flyers/HAGL2022_Day1.jpg';
+import day2 from './images/flyers/HAGL2022_Day2.jpg';
+import day3 from './images/flyers/HAGL2022_Day3.jpg';
+import day0 from './images/flyers/HAGL2022.jpg';
 
 const exampleInfo = {
   title: "ticket",
@@ -8,16 +14,21 @@ const exampleInfo = {
 }
 
 function Tickets() {
-  const ticket1 = ticketSection(exampleInfo);
-  const ticket2 = ticketSection(exampleInfo);
-  const ticket3 = ticketSection(exampleInfo);
-  const ticket4 = ticketSection(exampleInfo);
+  const t0 = ticketData.tickets[0];
+  const t1 = ticketData.tickets[1];
+  const t2 = ticketData.tickets[2];
+  const t3 = ticketData.tickets[3];
+
+  const ticket1 = ticketSection(t1);
+  const ticket2 = ticketSection(t2);
+  const ticket3 = ticketSection(t3);
+  const ticket0 = ticketSection(t0);
   const content = (
     <div className="tickets flex">
+      {ticket0}
       {ticket1}
       {ticket2}
       {ticket3}
-      {ticket4}
     </div>
   )
 
@@ -27,13 +38,40 @@ function Tickets() {
 }
 
 function ticketSection(ticketInfo) {
-  const { title, url, bands } = ticketInfo;
+  const { title, url, bands, description, price, day } = ticketInfo;
   const bandList = bandsList(bands);
+  let img;
+  let payment;
+  switch(day) {
+    default: img = null;
+      payment = null;
+      break;
+    case 0 : img = day0;
+      payment = tix(0);
+      break;
+    case 1: img = day1;
+      payment = tix(1);
+      break;
+    case 2: img = day2;
+      payment = tix(2);
+      break;
+    case 3: img = day3;
+      payment = tix(3);
+  }
   return (
     <div className={`ticket ${title}`}>
-      <h3>{title}</h3>
-      {bandList}
-      <a href={url}>{url}</a>
+      <img src={img} alt={title} />
+      <a href={img} target="_blank">
+        Download Flyer
+      </a>
+      <div>
+        <h3>{title}</h3>
+        <p>
+          {description}
+        </p>
+        {bandList}
+        {tix(url)}
+      </div>
     </div>
   )
 }
@@ -45,6 +83,19 @@ function bandsList(bands) {
         <li key={band}>{band}</li>
       ))}
     </ul>
+  )
+}
+
+function tix(url) {
+  return (
+    <div>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick"></input>
+        <input type="hidden" name="encrypted" value={url}></input>
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></input>
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+      </form>
+    </div>
   )
 }
 
