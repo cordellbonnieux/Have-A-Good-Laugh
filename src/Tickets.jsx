ef299625834cfd5f036c481ea229600cfbe4a9ec
Cordell <cordellbonnieux@gmail.com>
2023-02-05 12:33:55 -0800

    stupid js dates...

--- src/Tickets.jsx
+++ src/Tickets.jsx
@@ -8,49 +8,32 @@ function Tickets() {
   const ticketSalesEndDate = new Date(2023, 6, 15)
   const ticketSalesStartDate = new Date(2023, 2, 5)
 
-  let content
-  if (now < ticketSalesStartDate) {
-    content =  (
-      <div className="tickets">
-        <h3>On sale Feb 5th!</h3>
-      </div>
-    )
-  } else if (now > ticketSalesStartDate && now < ticketSalesEndDate) {
-    content = (
-      <div className="tickets">
-        <h3>HAGL2023: FULL PASS</h3>
-        <h4>Full access pass to all gigs listed on the schedule.</h4>
-        <p>
-          Two ways to pay: Paypal or E-Transfer.
-          Tickets are will call, bring government issued I.D. with you.
-          Waldorf shows are +19, Bullet Farm shows are all-ages.
-        </p>
-        <div id="paymentMethods">
-          <div className="paymentMethod">
-            <h4>Option #1: E-Transfer</h4>
-            <ul>
-              <li>Transfer $100 CAD/per ticket to thoughtdecayrecords@gmail.com</li>
-              <li>Put the full name of each ticket holder in the description</li>
-              <li>If no names are entered in the description we will refuse the payment</li>
-              <li>If your payment has gone through, your tickets have been added to the willcall list</li>
-              <li>If you have any problems, use the contact form below</li>
-            </ul>
-          </div>
-          <div className="paymentMethod">
-            {tix(url.url)}
-          </div>
+  let content = (
+    <div className="tickets">
+      <h3>HAGL2023: FULL PASS</h3>
+      <h4>Full access pass to all gigs listed on the schedule.</h4>
+      <p>
+        Two ways to pay: Paypal or E-Transfer.
+        Tickets are will call, bring government issued I.D. with you.
+        Waldorf shows are +19, Bullet Farm shows are all-ages.
+      </p>
+      <div id="paymentMethods">
+        <div className="paymentMethod">
+          <h4>Option #1: E-Transfer</h4>
+          <ul>
+            <li>Transfer $100 CAD/per ticket to thoughtdecayrecords@gmail.com</li>
+            <li>Put the full name of each ticket holder in the description</li>
+            <li>If no names are entered in the description we will refuse the payment</li>
+            <li>If your payment has gone through, your tickets have been added to the willcall list</li>
+            <li>If you have any problems, use the contact form below</li>
+          </ul>
         </div>
-      </div> 
-    )
-  } else {
-    // ticket sales are over
-    content = (
-      <div className="tickets">
-        <h3>HAGL2023 is over, check back often for future announcements!</h3>
+        <div className="paymentMethod">
+          {tix(url.url)}
+        </div>
       </div>
-    )
-  }
-
+    </div> 
+  )
 
   useEffect(() => {
     // make the tickets go boop boop
